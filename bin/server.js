9c8ef8b43b29a53de56836bb7fa80129e86eb1bb
Raphael Krausz <krauszraphael@gmail.com>
2016-10-28 18:06:24 +0200

    Added in routing tables to server.js - story router no longer controls all routes

--- bin/server.js
+++ bin/server.js
@@ -10,7 +10,11 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
 
-app.use(require('../src/story/story.routes.js'));
+// -- Load the routing tables --
+// Story routes
+app.use('/stories', require('../src/story/story.routes.js'));
+
+
 
 dotenv.load({ path: '.env' });
 
