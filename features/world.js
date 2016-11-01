9c7310cb57e5b284823ff293c6bf134a68a32db3
Raphael Krausz <krauszraphael@gmail.com>
2016-11-01 18:12:06 +0100

    Oops - adding in missing world.js file

---
+++ features/world.js
@@ -0,0 +1,27 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require(process.cwd() + '/bin/server');
+let expect = chai.expect;
+chai.use(chaiHttp);
+
+
+module.exports.World = function(callback) {
+
+  // var post;
+
+  this.send = undefined;
+  this.get  = undefined;
+
+
+  this.makeAndSendPost = function(route, data) {
+    var thePost = chai.request(server).post(route);
+    var theSend = thePost.send(data);
+    this.send = theSend;
+  }
+
+  this.makeGetRequest = function(route) {
+    this.get = chai.request(server).get(route);
+  }
+
+
+}
