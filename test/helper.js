cb52b4a20fcde78efb80d5cfeb3ce1138fbe80b1
Joao Pereira <joaopapereira@gmail.com>
2016-11-03 10:26:05 -0400

    Refactoring further

--- test/helper.js
+++ test/helper.js
@@ -2,7 +2,6 @@ let server = require(process.cwd() + '/bin/server');
 
 var mongoose = require('mongoose');
 
-
 var DatabaseCleaner = require('database-cleaner');
 var databaseCleaner = new DatabaseCleaner('mongodb');
 
@@ -11,18 +10,21 @@ let chaiHttp = require('chai-http');
 let should = chai.should();
 chai.use(chaiHttp);
 
+
 before(function (done) {
   mongoose.connection.on('connected', done);
 });
 
+cleanDatabase = function (callback) {
+  databaseCleaner.clean(mongoose.connections[0].db, function () {
+    callback();
+  });
+}
+
 module.exports = {
-    server,
-    request: function () {
-        return chai.request(server);
-    },
-    cleanDatabase: function (callback) {
-        databaseCleaner.clean(mongoose.connections[0].db, function () {
-            callback();
-        });
-    }
+  server,
+  request: function () {
+    return chai.request(server);
+  },
+  cleanDatabase: cleanDatabase
 };
--- test/story/story.routes.spec.js
+++ test/story/story.routes.spec.js
@@ -7,69 +7,66 @@ let request = helperFile.request;
 let cleanDatabase = helperFile.cleanDatabase;
 
 describe('(Router) Story', function () {
-    beforeEach(function (done) {
-        cleanDatabase(function () {
-            // console.log('Cleaned successfully');
-            var Story = require('../../src/story/story.model');
-            Story.create({
-                    url: "http://google.com",
-                    size: "1",
-                    name: "dummy entry"
-                },
-                function (err, story) {
-                    if (err) throw err;
-                    done();
-                }
-            );
+  beforeEach(function (done) {
+    cleanDatabase(function () {
+      var Story = require('../../src/story/story.model');
+      Story.create({
+          url: "http://google.com",
+          size: "1",
+          name: "dummy entry"
+        },
+        function (err, story) {
+          if (err) throw err;
+          done();
+        }
+      );
+    });
+  });
+
+  it('POST /stories', function (done) {
+    request()
+      .post('/stories')
+      .send({
+        url: 'https://github.com/AgileVentures/AsyncVoter/issues/4',
+        size: '3',
+        name: 'Start Vote Feature'
+      })
+      .end(function (err, res) {
+        res.should.have.status(200);
+        res.body.url.should.be.eql('https://github.com/AgileVentures/AsyncVoter/issues/4');
+        done()
+      });
+  });
+
+  it('GET /stories', function (done) {
+    request()
+      .get('/stories')
+      .end(function (err, res) {
+        res.should.have.status(200);
+        res.body.should.be.a('array');
+        res.body.length.should.be.eql(1);
+        done()
+      });
+  });
+
+  it('GET /stories/:id', function (done) {
+    var newStory = new Story({
+      url: 'https://github.com/AgileVentures/AsyncVoter/issues/5',
+      size: '1',
+      name: 'Receive Vote Feature'
+    });
+    newStory.save(function (err, data) {
+      request()
+        .get('/stories/' + data._id)
+        .end(function (err, res) {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('name');
+          res.body.should.have.property('url');
+          res.body.should.have.property('size');
+          res.body.should.have.property('_id').eql(res.body._id);
+          done();
         });
     });
-
-    it('POST /stories', function (done) {
-        request()
-            .post('/stories')
-            .send({
-                url: 'https://github.com/AgileVentures/AsyncVoter/issues/4',
-                size: '3',
-                name: 'Start Vote Feature'
-            })
-            .end(function (err, res) {
-                res.should.have.status(200);
-                // console.dir("hi!" + JSON.stringify(res.body))
-                res.body.url.should.be.eql('https://github.com/AgileVentures/AsyncVoter/issues/4');
-                done()
-            });
-    });
-
-    it('GET /stories', function (done) {
-        request()
-            .get('/stories')
-            .end(function (err, res) {
-                res.should.have.status(200);
-                res.body.should.be.a('array');
-                res.body.length.should.be.eql(1);
-                done()
-            });
-    });
-
-    it('GET /stories/:id', function (done) {
-        var newStory = new Story({
-            url: 'https://github.com/AgileVentures/AsyncVoter/issues/5',
-            size: '1',
-            name: 'Receive Vote Feature'
-        });
-        newStory.save(function (err, data) {
-            request()
-                .get('/stories/' + data._id)
-                .end(function (err, res) {
-                    res.should.have.status(200);
-                    console.log("Res body id" + res.body._id);
-                    res.body.should.be.a('object');
-                    res.body.should.have.property('name');
-                    res.body.should.have.property('url');
-                    res.body.should.have.property('size');
-                    res.body.should.have.property('_id').eql(res.body._id);
-                    done();
-                });
-        });
-    });
+  });
 });
