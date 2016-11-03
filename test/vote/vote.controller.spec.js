53e47fa5027425240466ac45215d6a8f0c36c079
Raphael Krausz <krauszraphael@gmail.com>
2016-11-03 13:43:02 +0100

    First castVote test passes

--- test/vote/vote.controller.spec.js
+++ test/vote/vote.controller.spec.js
@@ -15,14 +15,19 @@ var assert = require('chai').assert;
 // Test the casting of votes
 describe('Casting a vote', function () {
 
+  // Needed?
   var server = require(process.cwd() + '/bin/server');
 
   before(function (done) {
     console.log("In before(...)");
-    mongoose.connection.on('connected', function () {
+    // Something else goes here???
+
+    mongoose.connection.on('connected', function (err) {
       console.log("**** HERE I AM *****"); // Never reached!
       done();
     });
+
+
   });
 
   beforeEach(function (callback) {
@@ -38,26 +43,60 @@ describe('Casting a vote', function () {
 
 
   // We cast a vote of 2
-  // At the end we expect to get back a vote object with the same details as we said
-  // There should also be a vote entry in the database with the same details
+  // At the end we expect to be given a vote object with the same details as we said
   it('castVote - option 2 selected - testing response', function (done) {
-    var isssue = "https://github.com/AgileVentures/AsyncVoter/issues/7",
+    var issue = "https://github.com/AgileVentures/AsyncVoter/issues/7",
       developer = "Raphael Krausz",
       vote = 2;
-    console.log("++++ A");
+
     voteController.castVote(issue, developer, vote, function (err, theVote) {
-      console.log("++++ B");
-      if (err) done(err);
-      console.log("++++ C");
+
+      assert.isNotOk(err, 'Error returned');
+
+      if (err) return done(err);
+
+      assert.isOk(theVote, 'a Vote object was given');
+
+      assert.isOk(theVote.issue, 'a Vote object has issue set');
+      assert.isOk(theVote.developer, 'a Vote object has developer set');
+      assert.isOk(theVote.vote, 'a Vote object has vote set');
 
       assert(theVote.issue == issue, "issue sent matches response");
       assert(theVote.developer == developer, "developer sent matches response");
       assert(theVote.vote == vote, "vote sent matches response");
 
-      console.log("++++ D");
       done();
-      console.log("++++ E");
     });
   });
+
+
+  // We cast a vote of 2
+  // At the end we expect to get back a vote object with the same details as we said
+  // There should also be a vote entry in the database with the same details
+  it('castVote - option 2 selected - testing response', function (done) {
+    var issue = "https://github.com/AgileVentures/AsyncVoter/issues/7",
+      developer = "Raphael Krausz",
+      vote = 2;
+
+    voteController.castVote(issue, developer, vote, function (err, theVote) {
+
+      assert.isNotOk(err, 'Error returned');
+
+      if (err) return done(err);
+
+      assert.isOk(theVote, 'a Vote object was given');
+
+      assert.isOk(theVote.issue, 'a Vote object has issue set');
+      assert.isOk(theVote.developer, 'a Vote object has developer set');
+      assert.isOk(theVote.vote, 'a Vote object has vote set');
+
+      assert(theVote.issue == issue, "issue sent matches response");
+      assert(theVote.developer == developer, "developer sent matches response");
+      assert(theVote.vote == vote, "vote sent matches response");
+
+      done();
+    });
+  });
+
 
 });
