cc4b6b55f19b964b8bd739869244d9cd3e699310
Joao Pereira <joaopapereira@gmail.com>
2016-11-07 07:35:31 -0500

    Minor update

--- src/story/story.model.js
+++ src/story/story.model.js
@@ -11,6 +11,11 @@ var schema = new mongoose.Schema({
 schema.statics.findAllActive = function(cb) {
   return this.model('Story').find({size: 0}).sort('name').exec(cb);
 };
+schema.statics.findBy = function(filter, sort, callback) {
+	if(sort !== undefined) {
+		
+	}
+}
 
 
 var Story = mongoose.model('Story', schema);
