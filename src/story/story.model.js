0d1e6a80c70ff00a5e5985f73e4ddcf24298d092
Joao Pereira <joaopapereira@gmail.com>
2016-11-07 19:21:50 -0500

    Added tests and search types

--- src/story/story.model.js
+++ src/story/story.model.js
@@ -12,9 +12,27 @@ schema.statics.findAllActive = function(cb) {
   return this.model('Story').find({size: 0}).sort('name').exec(cb);
 };
 schema.statics.findBy = function(filter, sort, callback) {
+	sortArray = [];
 	if(sort !== undefined) {
-		
+		sort.split(',').forEach((sortOrder) => {
+			let sortOrderTrimmed = sortOrder.trim();
+			if(sortOrderTrimmed[0] === '-') {
+				sortArray.push([sortOrderTrimmed.substr(1, sortOrderTrimmed.length - 1), -1]);
+			} else {
+				sortArray.push(sortOrderTrimmed)
+			}
+		});
 	}
+	let filterObject = {};
+	if(filter !== undefined) {
+		filter.split(',').forEach((filterPair) => {
+			let filterPairSplit = filterPair.split('=');
+			if(filterPairSplit[0] === 'state') {
+				filterObject['size'] = 0;
+			}
+		});
+	}
+	return this.model('Story').find(filterObject).sort('name').exec(callback);
 }
 
 
---
+++ test/story/story.mode.spec.js
@@ -0,0 +1,112 @@
+var mongoose = require('mongoose');
+var Story = require(process.cwd() + '/src/story/story.model');
+
+var helperFile = require('../helper');
+let request = helperFile.request;
+let cleanDatabase = helperFile.cleanDatabase;
+let sinon = require('sinon');
+
+let chai = require('chai');
+let expect = chai.expect;
+
+describe('(Model) Story', function () {
+  let stub;
+  before(() => {
+    stub = sinon.stub(Story, "find");
+  })
+
+  describe('#findBy', function () {
+    describe ("filter", function() {
+      describe("by State", function () {
+        it("nothing found", function (done) {
+          let result = {
+            exec: (func) => func(undefined, []),
+            sort: (v) => {return result;} 
+          };
+          stub.withArgs({size: 0}).returns(result);
+          Story.findBy("state=active", undefined, done);
+          expect(stub.calledOnce).to.be.true;
+        });
+        it("found one active", function (done) {
+          let expectedResult = [new Story({name: 'story1', size: 0})];
+          let result = {
+            exec: (func) => func(undefined, expectedResult),
+            sort: (v) => {return result;} 
+          };
+          stub.withArgs({size: 0}).returns(result);
+          expect(Story.findBy("state=active", undefined, done)).to.be.eq(expectedResult);
+          expect(stub.calledOnce).to.be.true;
+        });
+      });
+    });
+    describe ("sort", function() {
+      describe("by name", function () {
+        it("nothing found", function (done) {
+          let result = {
+            exec: (func) => func(undefined, []),
+            sort: (v) => {return result;} 
+          };
+          stub.withArgs({}).returns(result);
+          expect(Story.findBy("", "name", done)).to.be.eq([]);
+          expect(stub.calledOnce).to.be.true;
+        });
+        it("found two", function (done) {
+          let expectedResult = [new Story({name: 'story1', size: 0}), new Story({name: 'story2', size: 1})];
+          let result = {
+            exec: (func) => func(undefined, [new Story({name: 'story2', size: 1}), new Story({name: 'story1', size: 0})]),
+            sort: (v) => {return result;} 
+          };
+          stub.withArgs({}).returns(result);
+          expect(Story.findBy("", "name", done)).to.be.eq(expectedResult);
+          expect(stub.calledOnce).to.be.true;
+        });
+        it("inverse order", function (done) {
+          let expectedResult = [new Story({name: 'story2', size: 1}), new Story({name: 'story1', size: 0})];
+          let result = {
+            exec: (func) => func(undefined, [new Story({name: 'story2', size: 1}), new Story({name: 'story1', size: 0})]),
+            sort: (v) => {return result;} 
+          };
+          stub.withArgs({}).returns(result);
+          expect(Story.findBy("", "-name", done)).to.be.eq(expectedResult);
+          expect(stub.calledOnce).to.be.true;
+        });
+      });
+      
+      describe("sort with multiple", function () {
+        it("nothing found", function (done) {
+          let result = {
+            exec: (func) => func(undefined, []),
+            sort: (v) => {return result;} 
+          };
+          stub.withArgs({}).returns(result);
+          expect(Story.findBy("", "name, size", done)).to.be.eq([]);
+          expect(stub.calledOnce).to.be.true;
+        });
+        it("found two", function (done) {
+          let expectedResult = [new Story({name: 'story1', size: 0}), new Story({name: 'story2', size: 1})];
+          let result = {
+            exec: (func) => func(undefined, [new Story({name: 'story2', size: 1}), new Story({name: 'story1', size: 0})]),
+            sort: (v) => {return result;} 
+          };
+          stub.withArgs({}).returns(result);
+          expect(Story.findBy("", "name,size", done)).to.be.eq(expectedResult);
+          expect(stub.calledOnce).to.be.true;
+        });
+        it("inverse order", function (done) {
+          let expectedResult = [new Story({name: 'story2', size: 1}), 
+                                new Story({name: 'story1', size: 0}),
+                                new Story({name: 'story1', size: 2})];
+          let result = {
+            exec: (func) => func(undefined, [new Story({name: 'story1', size: 2}),
+                                             new Story({name: 'story2', size: 1}), 
+                                             new Story({name: 'story1', size: 0})]),
+            sort: (v) => {return result;} 
+          };
+          stub.withArgs({}).returns(result);
+          expect(Story.findBy("", "-name, size", done)).to.be.eq(expectedResult);
+          expect(stub.calledOnce).to.be.true;
+        });
+      });
+    });
+  });
+});
