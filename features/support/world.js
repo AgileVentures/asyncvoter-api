6186ec6394a9e127d2cc7efca8ca68ef9d0a15e6
João Pereira <joaopapereira@gmail.com>
2016-11-09 10:30:25 -0500

    List Ongoing Votes Feature (#45)
    * Added multiple changes
    
    * Working tests
    
    * Added more steps
    
    * Added no votes active story
    
    * Corrected spec
    
    * Added order
    
    * Removed logic from controller
    
    * Minor update
    
    * Added tests and search types
    
    * Added tests
    
    * changes different error
    
    * Corrected spec name
    
    * Corrected issue in model
    
    * Corrected issue in model
    
    * Corrected issue
    
    * Corrected feature test
    
    * Removed sort
    
    * Remove function not in use
    
    * Corrected tests and  problem in PR
    
    * removes non-functioning filter stuff and tweak cukes

--- features/support/world.js
+++ features/support/world.js
@@ -2,6 +2,7 @@ let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require(process.cwd() + '/bin/server');
 chai.use(chaiHttp);
+let Story = require(process.cwd() + '/src/story/story.model');
 
 module.exports.World = function(callback) {
 
@@ -16,6 +17,31 @@ module.exports.World = function(callback) {
 
   this.makeGetRequest = function(route) {
     this.get = chai.request(server).get(route);
+    return this.get;
   }
 
+  this.createVotes = function(votes, done) {
+    Story.create(votes,
+        function (err, story) {
+            if (err) throw err;
+              done();
+        }
+    );
+  }
+  this.strToIndex = function(str) {
+    switch(str) {
+      case 'first':
+        return 0;
+      case 'second':
+        return 1;
+      case 'third':
+        return 2;
+      case 'fourth':
+        return 3;
+      case 'fifth':
+        return 4;
+      default:
+        return str;
+    }
+  }
 }
