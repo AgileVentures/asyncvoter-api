b116d9b6b441b86d921eb70a1b3ff481831eba4b
Chaiwa Berian <chaiwa.berian@gmail.com>
2016-11-16 22:36:49 +0200

    Add reference to story model in World.js

--- features/step_definitions/show_votes_on_story.js
+++ features/step_definitions/show_votes_on_story.js
@@ -1,16 +1,13 @@
 var assert = require('chai').assert;
 
-var foo = 'bar';
 
-assert.lengthOf(foo,3,'foo length is 3');
 
 this.Given(/^there is a story with votes$/, function (callback) {
   // Write code here that turns the phrase above into concrete actions
   callback(null, 'pending');
 });
 
-this.Then(/^the response should be a list of votes on that story$/, funct
-ion (callback) {
+this.Then(/^the response should be a list of votes on that story$/, function (callback) {
   // Write code here that turns the phrase above into concrete actions
   callback(null, 'pending');
 });
--- features/support/world.js
+++ features/support/world.js
@@ -3,6 +3,7 @@ let chaiHttp = require('chai-http');
 let server = require(process.cwd() + '/bin/server');
 chai.use(chaiHttp);
 let Story = require(process.cwd() + '/src/story/story.model');
+let Vote = require(process.cwd() + '/src/vote/vote.model');
 
 module.exports.World = function (callback) {
 
