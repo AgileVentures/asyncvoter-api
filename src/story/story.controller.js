2b9f9518b67e8850bbd7f4403897a99f0b89e121
user <arreche@gmail.com>
2016-11-21 13:27:38 +0000

    Remove the logging functions

--- src/story/story.controller.js
+++ src/story/story.controller.js
@@ -11,10 +11,9 @@ exports.createStory = function (req, res, next) {
     var name = req.body.name;
     var size = req.body.size;
     var url = req.body.url;
-    // console.log("Here is the request body: " + JSON.stringify(req.body))
+
     Story.create({"name": name, "size": size, "url": url}, function(err, story) {
         if (err) throw err;
-        // console.log('Story created!');
         var id = story._id;
         res.send(story);
     });
