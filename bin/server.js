0d979c0ecc65d385b2b70b84a8dd199c33c6c19e
Sam Joseph <tansaku@gmail.com>
2016-12-01 15:12:26 +0000

    rearranges middleware to make it easier to see what is being used

--- bin/server.js
+++ bin/server.js
@@ -1,16 +1,18 @@
 var express = require('express');
-var cors = require('cors');
 var app = express();
-app.use(cors());
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var dotenv = require('dotenv');
-var chalk = require('chalk');
 
-app.use( bodyParser.json() );
+var cors = require('cors');
+var bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
+
+var mongoose = require('mongoose');
+var dotenv = require('dotenv');
+var chalk = require('chalk');
 
 // Load the routing tables
 require('../src/routes')(app)
