88feeb6cc93bdf5da859912e852a65b7e693d9a6
user <arreche@gmail.com>
2016-12-01 12:27:53 +0000

    test CORS is not restricted to any domain

--- test/cors.spec.js
+++ test/cors.spec.js
@@ -1,19 +1,19 @@
 let helper = require('./helper');
 
 describe('CORS', function () {
-  it('GET /stories should allow cross origin', function(done) {
+  it('GET /stories should allow cross origin from anywhere', function(done) {
     helper.request()
           .get('/stories')
           .end(function(err, res) {
-            res.should.to.have.header('Access-Control-Allow-Origin');
+            res.should.to.have.header('Access-Control-Allow-Origin', '*');
             done();
     });
   });
-  it('GET /stories/:id/votes should allow cross origin', function(done) {
+  it('GET /stories/:id/votes should allow cross origin from anywhere', function(done) {
     helper.request()
           .get('/stories/1/votes')
           .end(function(err, res) {
-            res.should.to.have.header('Access-Control-Allow-Origin');
+            res.should.to.have.header('Access-Control-Allow-Origin', '*');
             done();
     });
   });
